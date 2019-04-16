39ce72c9b0c9a581b307bb392b7e3adb605c6608
Dina Malone <dina.malone@newswhip.com>
2019-04-16 11:29:40 +0100

    stream sample presentation over http

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var pptx = require("pptxgenjs");
+var PptxGenJS = require("pptxgenjs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,8 +8,35 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/generate-pptx', function (req, res) {
-    let data = req.body.data;
-    res.send({message: 'POST request to /generate-pptx', data})
+    // let data = req.body.data;
+    const pptx = new PptxGenJS();
+    const slide = pptx.addNewSlide();
+
+    slide.addText('Hello World!', { x:1.5, y:1.5, fontSize:18, color:'363636' });
+
+    // slide.addImage({path: 'images/feedback.png'});
+
+    slide.addChart(pptx.charts.BAR, [{
+            name  : 'Actual Sales',
+            labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+            values: [1500, 4600, 5156, 3167, 8510, 8009, 6006, 7855, 12102, 12789, 10123, 15121]
+        },
+        {
+            name  : 'Projected Sales',
+            labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+            values: [1000, 2600, 3456, 4567, 5010, 6009, 7006, 8855, 9102, 10789, 11123, 12121]
+        }], { x:1.0, y:1.0, w:12, h:6 } );
+
+    slide.addTable([
+        { text:'Cell 1 A',  options:{fontFace:'Arial'  } },
+        { text:'Cell 1 B',  options:{fontFace:'Courier'} }
+    ], { x:0.5, y:5, w:9, h:2, colW:[1.5,1.5,6] });
+
+    // Will return BufferArray. https://gitbrent.github.io/PptxGenJS/docs/usage-saving.html#nodejs
+    pptx.save('http', function(file) {
+        res.send({pptx: file})
+    });
+
 });
 
 module.exports = router;
